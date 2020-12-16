cc4d198a7bdd65d4bd6e246ff8af25c79406709c
nrck6228 <47654183+nrck6228@users.noreply.github.com>
2020-12-16 11:17:08 +0700

    Rewrite HTML

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -132,43 +132,5 @@
 
 
 @include('template1/include.css_scripts')
-<!-- Gallery -->
-<link rel="stylesheet" href="{{ asset('gallery/lightgallery.css')}}">
-<script type="text/javascript" src="{{ asset('gallery/lightgallery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('gallery/lg-thumbnail.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('gallery/lg-fullscreen.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('gallery/lg-video.min.js') }}"></script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#lightgallery').lightGallery({
-            thumbnail:true
-        });
-    });
-</script>
-<script>
-    $('.icon-like').on('click', function(event, count) {
-        event.preventDefault();
-        
-        var $this = $(this),
-            count = $this.attr('data-count'),
-            active = $this.hasClass('active'),
-            multiple = $this.hasClass('multiple-count');
-        
-        // First method, allows to add custom function
-        // Use when you want to do an ajax request
-        /* if (multiple) {
-        $this.attr('data-count', ++count);
-        // Your code here
-        } else {
-        $this.attr('data-count', active ? --count : ++count).toggleClass('active');
-        // Your code here
-        } */
-        
-        // Second method, use when ... I dunno when but it looks cool and that's why it is here
-        $.fn.noop = $.noop;
-        $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
-        
-    });
-</script>
 @endsection
 

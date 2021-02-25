5962e57b01343f2a63df4748c9dcef97e976447b
anurakball <anurakball@gmail.com>
2021-02-25 15:36:52 +0700

    ...

--- resources/views/template1/calendar.blade.php
+++ resources/views/template1/calendar.blade.php
@@ -3,10 +3,9 @@
                 <div class="group-mange-section">
                     <div class="manage-tools">
                         <ul>
-                            <li class="order-list">จัดการ</li>
-                            <li class="order-list">ลบ</li>
-                            <li class="order-list">ซ่อน</li>
-                            <li class="order-list order-close">ปิด</li>
+                            <li class="order-list"><a style="color:#000000" href="#" data-toggle="modal" data-target="#modal_full" onclick="sfi('กำลังโหลด...','/manage-admin/add?m=activities');">เพิ่มข้อมูล</a></li>
+                            <li class="order-list"><a style="color:#000000" href="#" data-toggle="modal" data-target="#modal_full" onclick="sfi('กำลังโหลด...','/manage-admin/list?m=activities');">บริหารจัดการ</a></li> 
+                            <li class="order-list order-close">ปิด</li> 
                         </ul>
                     </div> 
                     <div class="manage-dragdrop">
@@ -21,7 +20,7 @@
                         <div class="top-content">
                             <div class="group-text">
                                 <h3 class="title color-primary">ปฏิทินกิจกรรม</h3>
-                                <p class="desc color-secondary">วันนี้ : 2 / พฤศจิกายน / 2563</p>
+                                <p class="desc color-secondary">The Bangkok Metropolitan Council Secretariat</p>
                             </div>
                             <div class="group-gotoall">
                                 <a href="/calendar" class="btn btn-outline-dark">กิจกรรมทั้งหมด</a>
@@ -33,18 +32,18 @@
                                     <div class="select-wrapper">
                                         <select name="month" class="slc">
                                             <optgroup label="เดือน">
-                                                <option value="">มกราคม</option>
-                                                <option value="">กุมภาพันธ์</option>
-                                                <option value="">มีนาคม</option>
-                                                <option value="">เมษายน</option>
-                                                <option value="">พฤษภาคม</option>
-                                                <option value="">มิถุนายน</option>
-                                                <option value="">กรกฎาคม</option>
-                                                <option value="">สิงหาคม</option>
-                                                <option value="">กันยายน</option>
-                                                <option value="">ตุลาคม</option>
-                                                <option value="">พฤศจิกายน</option>
-                                                <option value="">ธันวาคม</option>
+                                                <option value="1">มกราคม</option>
+                                                <option value="2">กุมภาพันธ์</option>
+                                                <option value="3">มีนาคม</option>
+                                                <option value="4">เมษายน</option>
+                                                <option value="5">พฤษภาคม</option>
+                                                <option value="6">มิถุนายน</option>
+                                                <option value="7">กรกฎาคม</option>
+                                                <option value="8">สิงหาคม</option>
+                                                <option value="9">กันยายน</option>
+                                                <option value="10">ตุลาคม</option>
+                                                <option value="11">พฤศจิกายน</option>
+                                                <option value="12">ธันวาคม</option>
                                             </optgroup>
                                         </select>
                                     </div>
@@ -58,86 +57,33 @@
                             
                             <div class="group-slide">
                                 <div class="list-item" id="slide_1">
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                                                <div class="left">
-                                                    <div class="text-content bg-color-primary">2</div>
-                                                    <div class="text-onbottom bg-color-secondary">พ.ย.</div>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/calendar-detail">
-                                                            บึงกุ่มชวนร่วมกิจกรรมบ้านหนังสือ
-                                                        </a>
-                                                    </div>
-                                                    <div class="text-onbottom"><i class="fas fa-map-marker-alt"></i> ถนนเสรีไทย ฝั่งเลขคู่</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                                                <div class="left">
-                                                    <div class="text-content bg-color-primary">3</div>
-                                                    <div class="text-onbottom bg-color-secondary">พ.ย.</div>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/calendar-detail">
-                                                            ตรวจพื้นที่ถนนพระรามที่ 4 ตั้งแต่แยกกล้วยน้ำไทถึงแยกพระโขนง
-                                                        </a>
-                                                    </div>
-                                                    <div class="text-onbottom"><i class="fas fa-map-marker-alt"></i> ถนนเสรีไทย ฝั่งเลขคู่</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                                                <div class="left">
-                                                    <div class="text-content bg-color-primary">2</div>
-                                                    <div class="text-onbottom bg-color-secondary">พ.ย.</div>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/calendar-detail">
-                                                            บึงกุ่มชวนร่วมกิจกรรมบ้านหนังสือ
-                                                        </a>
-                                                    </div>
-                                                    <div class="text-onbottom"><i class="fas fa-map-marker-alt"></i> ถนนเสรีไทย ฝั่งเลขคู่</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                                                <div class="left">
-                                                    <div class="text-content bg-color-primary">3</div>
-                                                    <div class="text-onbottom bg-color-secondary">พ.ย.</div>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/calendar-detail">
-                                                            ตรวจพื้นที่ถนนพระรามที่ 4 ตั้งแต่แยกกล้วยน้ำไทถึงแยกพระโขนง
-                                                        </a>
-                                                    </div>
-                                                    <div class="text-onbottom"><i class="fas fa-map-marker-alt"></i> ถนนเสรีไทย ฝั่งเลขคู่</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+<?php
+foreach(${"data_box_".$r->box_number} as $r_sub)
+{
+    ?>
+    <div class="item">
+        <div class="row">
+            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                <div class="left">
+                    <div class="text-content bg-color-primary">2</div>
+                    <div class="text-onbottom bg-color-secondary">พ.ย.</div>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                <div class="right">
+                    <div class="text-content">
+                        <a href="/calendar-detail">
+                            <?php echo $CustomHelper->L($r_sub->title,$r_sub->en_title); ?>
+                        </a>
+                    </div>
+                    <!--<div class="text-onbottom"><i class="fas fa-map-marker-alt"></i> ถนนเสรีไทย ฝั่งเลขคู่</div>-->
+                </div>
+            </div>
+        </div>
+    </div> 
+    <?php
+}
+?> 
                                 </div>
                             </div>
                         </div>
--- resources/views/template1/faq.blade.php
+++ resources/views/template1/faq.blade.php
@@ -3,9 +3,8 @@
                 <div class="group-mange-section">
                     <div class="manage-tools">
                         <ul>
-                            <li class="order-list">จัดการ</li>
-                            <li class="order-list">ลบ</li>
-                            <li class="order-list">ซ่อน</li>
+                            <li class="order-list"><a style="color:#000000" href="#" data-toggle="modal" data-target="#modal_full" onclick="sfi('กำลังโหลด...','/manage-admin/add?m=faq');">เพิ่มข้อมูล</a></li>
+                            <li class="order-list"><a style="color:#000000" href="#" data-toggle="modal" data-target="#modal_full" onclick="sfi('กำลังโหลด...','/manage-admin/list?m=faq');">บริหารจัดการ</a></li> 
                             <li class="order-list order-close">ปิด</li>
                         </ul>
                     </div> 
@@ -36,144 +35,41 @@
                             </div>
                             
                             <div class="group-slide">
-                                <div class="list-item" id="slide_2">
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-lg-4 col-md-4 col-sm-4">
-                                                <div class="left">
-                                                    <div class="icon-content bg-color-primary">
-                                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                                            width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
-                                                        <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
-                                                            c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-8 col-md-8 col-sm-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/faq">
-                                                        เด็กอายุกี่ขวบต้องแจ้งการเข้าเรียนตามกฎหมาย และใช้เอกสารอะไรบ้าง?
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-lg-4 col-md-4 col-sm-4">
-                                                <div class="left">
-                                                    <div class="icon-content bg-color-primary">
-                                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                                            width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
-                                                        <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
-                                                            c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-8 col-md-8 col-sm-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/faq">
-                                                        เด็กอายุกี่ขวบต้องแจ้งการเข้าเรียนตามกฎหมาย และใช้เอกสารอะไรบ้าง?
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="item">
-                                        <div class="row">
-                                            <div class="col-lg-4 col-md-4 col-sm-4">
-                                                <div class="left">
-                                                    <div class="icon-content bg-color-primary">
-                                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                                            width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
-                                                        <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
-                                                            c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-8 col-md-8 col-sm-8">
-                                                <div class="right">
-                                                    <div class="text-content">
-                                                        <a href="/faq">
-                                                        เด็กอายุกี่ขวบต้องแจ้งการเข้าเรียนตามกฎหมาย และใช้เอกสารอะไรบ้าง?
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div class="list-item" id="slide_2"> 
+<?php
+foreach(${"data_box_".$r->box_number} as $r_sub)
+{
+    ?>
+    <div class="item">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="left">
+                    <div class="icon-content bg-color-primary">
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
+                        <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
+                            c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-8 col-sm-8">
+                <div class="right">
+                    <div class="text-content">
+                        <a href="/faq">
+                        <?php echo $CustomHelper->L($r_sub->title,$r_sub->en_title); ?>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
+?>
+ 
                                 </div>
-                                <!-- <ul id="slide_2" class="list-item">
-                                    <li>
-                                        <div class="item">
-                                            <div class="left">
-                                                <div class="icon-content bg-color-primary">
-                                                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                                        width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
-                                                    <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
-                                                        c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                            <div class="right">
-                                                <div class="text-content">
-                                                    <a href="">
-                                                        เด็กอายุกี่ขวบต้องแจ้งการเข้าเรียนตามกฎหมาย และใช้เอกสารอะไรบ้าง?
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="item">
-                                            <div class="left">
-                                                <div class="icon-content bg-color-primary">
-                                                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                                        width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
-                                                    <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
-                                                        c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                            <div class="right">
-                                                <div class="text-content">
-                                                    <a href="">
-                                                        เด็กอายุกี่ขวบต้องแจ้งการเข้าเรียนตามกฎหมาย และใช้เอกสารอะไรบ้าง?
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="item">
-                                            <div class="left">
-                                                <div class="icon-content bg-color-primary">
-                                                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                                        width="42.2px" height="42.2px" viewBox="0 0 42.2 42.2" style="enable-background:new 0 0 42.2 42.2;" xml:space="preserve">
-                                                    <path class="st0" d="M5.1,21c0,8.6,6.9,15.5,15.4,16c5.7,0.4,11.4-1.3,16.5-3.9c-2.8-2.8-2.1-6.8-1.3-9.6c1.1-3.3,1.1-7-0.5-10.1
-                                                        c-1.1-2.1-2.7-4-4.6-5.4c-0.7-0.6-2.4-1.4-2.4-1.4c-2.1-1-4.5-1.6-7-1.6c-9,0-16.2,7.3-16.2,16.2V21z"/>
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                            <div class="right">
-                                                <div class="text-content">
-                                                    <a href="">
-                                                        เด็กอายุกี่ขวบต้องแจ้งการเข้าเรียนตามกฎหมาย และใช้เอกสารอะไรบ้าง?
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul> -->
+                                
                             </div>
                         </div>
                     </div>

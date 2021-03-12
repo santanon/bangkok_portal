e84eeaa9e5bcc81b11f74570ab12b8c9f84a560a
tob <tob@ecommerce.co.th>
2021-03-12 18:04:00 +0700

    ีupdateTP2-3

--- resources/views/template2/main-slide.blade.php
+++ resources/views/template2/main-slide.blade.php
@@ -18,7 +18,7 @@
                         </div>
                     </a>
                 </div>
-                <ul class="list-banner">
+                <!-- <ul class="list-banner">
                     <li>
                         <div class="bg-layer"></div>
                         <div class="banner" style="background-image: url('../../template2/assets/images/banner/img-banner-demo.png');"></div>
@@ -64,7 +64,60 @@
                             </div>
                         </div>
                     </li>
-                </ul>
+                </ul> -->
+
+
+                <!-- Swiper -->
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide effect">
+                            <div class="group-banner-img">
+                                <div class="banner" style="background-image: url('../../template2/assets/images/banner/img-banner-demo.png');"></div>
+                                <div class="bg-slide">
+                                    <img src="{{ asset('assets/images/noise.png')}}" alt="Noise" title="Noise">
+                                </div>
+                                <div class="item-Onbanner-outer">
+                                    <div class="item-Onbanner-inner">
+                                        <div class="list">
+                                            <h2 class="title-banner">กรุงเทพมหานครเมืองหลวงของประเทศไทย</h2>
+                                            <p class="desc-banner">มุ่งมั่นการทำงาน แหล่งค้นคว้าการประชุม เครือข่ายมหานครอาเซียน</p>
+                                            <div class="action-link">
+                                                <a href="#">รายละเอียดเพิ่มเติม</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide effect">
+                            <div class="group-banner-img">
+                                <div class="banner" style="background-image: url('../../template2/assets/images/banner/img-banner-demo.png');"></div>
+                                <div class="bg-slide">
+                                    <img src="{{ asset('assets/images/noise.png')}}" alt="Noise" title="Noise">
+                                </div>
+                                <div class="item-Onbanner-outer">
+                                    <div class="item-Onbanner-inner">
+                                        <div class="list">
+                                            <h2 class="title-banner">กรุงเทพมหานครเมืองหลวงของประเทศไทย</h2>
+                                            <p class="desc-banner">มุ่งมั่นการทำงาน แหล่งค้นคว้าการประชุม เครือข่ายมหานครอาเซียน</p>
+                                            <div class="action-link">
+                                                <a href="#">รายละเอียดเพิ่มเติม</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- Add Arrows -->
+                    <div class="swiper-button-next swiper-button-white"></div>
+                    <div class="swiper-button-prev swiper-button-white"></div>
+                </div>
+                
             </div>
         </div>
     </div>
